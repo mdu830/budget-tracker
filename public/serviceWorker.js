0305f5886c038d08fb121aec11d68b38aa486709
mdu830 <mdu830@yahoo.com>
2021-04-14 17:04:21 -0400

    minor changes to service worker

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -13,8 +13,8 @@ const FILES_TO_CACHE = [
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
 
-self.addEventListener("install", (evt) => {
-    evt.waitUntil(
+self.addEventListener("install", (event) => {
+    event.waitUntil(
       caches.open(CACHE_NAME).then((cache) => {
         return cache.addAll(FILES_TO_CACHE);
       })
@@ -23,22 +23,22 @@ self.addEventListener("install", (evt) => {
     self.skipWaiting();
 });
 
-self.addEventListener("fetch", (evt) => {
-    if (evt.request.url.includes("/api/") && evt.request.method === "GET") {
-        evt.respondWith(
+self.addEventListener("fetch", (event) => {
+    if (event.request.url.includes("/api/") && event.request.method === "GET") {
+        event.respondWith(
             caches
             .open(DATA_CACHE_NAME)
             .then((cache) => {
-                return fetch(evt.request)
+                return fetch(event.request)
                 .then((response) => {
 
                     if (response.status === 200) {
-                    cache.put(evt.request, response.clone());
+                    cache.put(event.request, response.clone());
                     }
                     return response;
                 })
                 .catch(() => {
-                    return cache.match(evt.request);
+                    return cache.match(event.request);
                 });
             })
             .catch((err) => console.log(err))
@@ -46,9 +46,9 @@ self.addEventListener("fetch", (evt) => {
         return;
     }
 
-    evt.respondWith(
-        caches.match(evt.request).then((response) => {
-          return response || fetch(evt.request);
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+          return response || fetch(event.request);
         })
     );
 });
